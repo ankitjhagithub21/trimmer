ed9d9fc8dd9246c2091f92e0f9d214db8aebc9a0
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-06 08:21:27 +0530

    created header and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer>
-            footer
-    </footer>
-  )
+    return (
+        <footer className="footer footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by Ankit Jha</p>
+            </aside>
+        </footer>
+    )
 }
 
 export default Footer
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-   <header>
-    header
-   </header>
+    <header>
+      <div className="navbar bg-base-100 container mx-auto">
+        <div className="flex-1">
+          <Link to={"/"} className="btn btn-ghost text-xl">Trimmer</Link>
+        </div>
+        <div className="flex-none gap-2">
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+             
+              <li><Link to={"/links"}>My Links</Link></li>
+            <li>
+            <button className='text-left mt-2'>Logout</button>
+            </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
