0830b5f8e0cf82c7229b3cae6cb03f21a9823ee5
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-06 14:07:52 +0530

    created login and signup page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const user = null
@@ -11,25 +11,25 @@ const Header = () => {
         {
           user ? <div className="flex-none gap-2">
 
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
               </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+
+                <li><Link to={"/links"}>My Links</Link></li>
+                <li>
+                  <button className='text-left mt-2'>Logout</button>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-             
-              <li><Link to={"/links"}>My Links</Link></li>
-            <li>
-            <button className='text-left mt-2'>Logout</button>
-            </li>
-            </ul>
-          </div>
-        </div> : <button className='btn btn-primary'>Login</button>
+          </div> : <Link to={"/auth"} className='btn btn-primary'>Login</Link>
         }
       </div>
     </header>
--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import Login from '../components/login'
+import Signup from '../components/signup'
 
 const Auth = () => {
   const [searchParams] = useSearchParams()
   const url = searchParams.get('createNew')
+  const [currPage, setCurrPage] = useState("login")
+
   return (
-    <div>
-      {
-        url  ? <div>Hold Up ! Let's Login First..</div> : <div>Login/SignUp</div>
+    <section className='py-12'>
+      <h1 className='text-center md:text-5xl text-2xl font-bold mb-10'>
+        {
+          url ? "Hold Up ! Let's Login First.." : 'Login/Signup'
       }
-    </div>
+      </h1>
+      <div className='flex items-center justify-center'>
+       {
+         ["login","signup"].map((btn,index)=>{
+          return <button className={`btn md:btn-wide  ${btn===currPage && 'btn-primary'}`} key={index} onClick={()=>setCurrPage(btn)}>{btn}</button>
+        })
+       }
+      </div>
+      <div className='max-w-lg mx-auto my-12'>
+        {
+          currPage === "login" ? <Login /> : <Signup />
+        }
+      </div>
+
+    </section>
   )
 }
 
